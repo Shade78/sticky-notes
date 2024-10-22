a5dd5961634bebbb4379d09760fd031cf76124e4
Shade78 <shademoon322@gmail.com>
2024-10-23 00:50:17 +0300

    added drag and drop
    DnD on cards with limit space to left and top,
    and focus on active card

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,26 +1,59 @@
-import { useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import Trash from "../icons/Trash";
+import { setNewOffset, autoGrow, setZIndex } from "../utils";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
-  const position = JSON.parse(note.position);
+  const [position, setPosition] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
 
+  const mouseStartPos = { x: 0, y: 0 };
+
+  const cardRef = useRef(null);
   const textAreaRef = useRef(null);
 
   useEffect(() => {
     autoGrow(textAreaRef);
   }, []);
 
-  const autoGrow = (textAreaRef) => {
-    const { current } = textAreaRef;
-    current.style.height = "auto";
-    current.style.height = current.scrollHeight + "px";
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+
+    setZIndex(cardRef.current);
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
+  const mouseMove = (e) => {
+    const mouseMoveDirection = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    const newPosition = setNewOffset(cardRef.current, mouseMoveDirection);
+
+    setPosition(newPosition);
+
+    // setPosition({
+    //   x: cardRef.current.offsetLeft - mouseMoveDirection.x,
+    //   y: cardRef.current.offsetTop - mouseMoveDirection.y,
+    // });
   };
 
   //TODO change to styled-components
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -31,6 +64,7 @@ const NoteCard = ({ note }) => {
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
+        onMouseDown={mouseDown}
       >
         <Trash />
       </div>
@@ -40,6 +74,7 @@ const NoteCard = ({ note }) => {
           defaultValue={body}
           style={{ color: colors.colorText }}
           onInput={() => autoGrow(textAreaRef)}
+          onFocus={() => setZIndex(cardRef.current)}
         ></textarea>
       </div>
     </div>
