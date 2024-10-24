0627d6cc2cef55bfe6a0e549c01d5935b59217f0
Shade78 <shademoon322@gmail.com>
2024-10-25 01:08:57 +0300

    loaded notes from database
    also added body parser utility
    and databases queries

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,8 +1,28 @@
-import React from "react";
-import { fakeData as notes } from "../assets/fakeData";
+import { useState, useEffect } from "react";
+// import { fakeData as notes } from "../assets/fakeData";
+import { db } from "../appwrite/databases";
+import { databases } from "../appwrite/config";
 import NoteCard from "../components/NoteCard";
 
 const NotesPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const response = await db.notes.list();
+
+    // worse readability
+    // const response = await databases.listDocuments(
+    //   import.meta.env.VITE_DATABASE_ID,
+    //   import.meta.env.VITE_COLLECTION_NOTES_ID
+    // );
+
+    setNotes(response.documents);
+  };
+
   return (
     <div>
       {notes.map((note) => (
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+// prevents card (note) from being moved beyond left and top borders
 export const setNewOffset = (card, mouseMoveDir = { x: 0, y: 0 }) => {
   const offsetLeft = card.offsetLeft - mouseMoveDir.x;
   const offsetTop = card.offsetTop - mouseMoveDir.y;
@@ -8,12 +9,14 @@ export const setNewOffset = (card, mouseMoveDir = { x: 0, y: 0 }) => {
   };
 };
 
+// expand card (note)
 export const autoGrow = (textAreaRef) => {
   const { current } = textAreaRef;
   current.style.height = "auto";
   current.style.height = current.scrollHeight + "px";
 };
 
+// setting the card (note) to the foreground
 export const setZIndex = (selectedCard) => {
   selectedCard.style.zIndex = 999;
 
@@ -22,4 +25,13 @@ export const setZIndex = (selectedCard) => {
       card.style.zIndex = selectedCard.style.zIndex - 1;
     }
   });
+};
+
+// DB may contain plain text so no need to always call JSON.parse
+export const bodyParser = (value) => {
+  try {
+    return JSON.parse(value);
+  } catch (error) {
+    return value;
+  }
 };
